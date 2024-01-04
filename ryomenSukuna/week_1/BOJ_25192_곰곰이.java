eb7e23e5647af0fccd01dfd9277d7c7e930b8b34
Taaate <an981022@naver.com>
2024-01-04 14:45:47 +0900

    1/4

---
+++ ryomenSukuna/week_1/BOJ_25192_곰곰이.java
@@ -0,0 +1,29 @@
+package week_1;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class BOJ_25192_곰곰이 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int count = 0;
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            String input = in.next();
+            if (input.equals("ENTER")) {
+                count += set.size();
+                set = new HashSet<>();
+            } else {
+                set.add(input);
+            }
+        }
+        count += set.size();
+
+        System.out.println(count);
+    }
+}
