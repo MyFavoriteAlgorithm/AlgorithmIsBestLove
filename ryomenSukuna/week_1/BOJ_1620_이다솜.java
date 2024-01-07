e20fab7348c61113d1b4c9d5fe7cd74618f2b343
Taaate <an981022@naver.com>
2024-01-07 13:05:17 +0900

    1/7

---
+++ ryomenSukuna/week_1/BOJ_1620_이다솜.java
@@ -0,0 +1,35 @@
+package week_1;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class BOJ_1620_이다솜 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+        Map<Integer, String> hash1 = new HashMap<>();
+        Map<String, Integer> hash2 = new HashMap<>();
+
+
+        for(int i = 1; i <= n; i++) {
+            String S = in.next();
+            hash1.put(i, S);
+            hash2.put(S, i);
+        }
+
+        for(int i = 0; i < m; i++) {
+            String S = in.next();
+            if(49 <= S.charAt(0) && S.charAt(0) <= 57) {
+                sb.append(hash1.get(Integer.parseInt(S))).append("\n");
+            }else {
+                sb.append(hash2.get(S)).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
---
+++ ryomenSukuna/week_1/BOJ_1764_듣보잡.java
@@ -0,0 +1,44 @@
+package week_1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class BOJ_1764_듣보잡 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0 ; i < n; i ++) {
+            String input = in.next();
+            map.put(input, map.getOrDefault(input, 0) + 1);
+        }
+
+        for (int i = 0 ; i < m; i ++) {
+            String input = in.next();
+            map.put(input, map.getOrDefault(input, 0) + 1);
+        }
+
+        List<String> names = new ArrayList<>();
+
+        for (String s : map.keySet()) {
+            if (map.get(s) == 2) {
+                names.add(s);
+            }
+        }
+
+        Collections.sort(names);
+
+        System.out.println(names.size());
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
